6be9fc30a65f1fe8ce5a2ee51a737bac52abfbae
Eric Yang <eric.yang@poxst.com>
2024-10-31 19:31:11 +1100

    init

---
+++ Sources/SwiftWhisper/Segment.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+public struct Segment: Equatable {
+    public let startTime: Int
+    public let endTime: Int
+    public let text: String
+    public let probability: Float
+}
